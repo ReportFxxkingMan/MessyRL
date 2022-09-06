30fa3cc549e4529c3125e37ef8dd7c2cd23b0c2e
SungChul Hong <chulhongsung@gmail.com>
2022-09-06 20:12:53 +0900

    add RecordEpisode class

---
+++ utils/record.py
@@ -0,0 +1,48 @@
+import gym
+import tensorflow as tf
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+env = gym.make()
+
+class RecordEpisode():
+
+    def __init__(
+        self,
+        env,
+        actor
+    ):  
+        """        
+        Args:
+            env (str): open-ai gym environment ex) bipedalwalkr-v3
+            agent (tf.keras.models.Model): agent actor model has state as input and action array as output ex) pdpg.actor
+        """
+    
+        self.env = env 
+        self.actor = actor
+    
+    def record(self, path):
+        """
+        Recording an episode video 
+
+        Args:
+            path (str): Directory path for recorded video saving
+        """
+        state = env.reset()
+
+        video_recorder = VideoRecorder(self.env, path, enabled=True)
+        while not done:
+            
+            env.render()
+            video_recorder.capture_frame()
+            action = self.actor(tf.convert_to_tensor([state], dtype=tf.float32))
+            action = action.numpy()[0]
+            
+            next_state, reward, done, _ = env.step(action)
+            
+            state = next_state
+            episode_reward += reward
+            time += 1  
+
+        video_recorder.close()
+        video_recorder.enabled = False
+        env.close()        
