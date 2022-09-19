7a247035454ee1ab0b4ef85ef69b753527085dd5
RektPunk <rektpunk@gmail.com>
2022-09-19 23:17:55 +0900

    feat - change buffer size as capacity

--- module/common/replaybuffer.py
+++ module/common/replaybuffer.py
@@ -26,8 +26,8 @@ class ReplayBuffer(AbstractBuffer):
             capacity (int, optional): Max size of this buffer. Defaults to 10000.
         """
         self.buffer = deque(maxlen=capacity)
+        self.capacity = capacity
         self.buffer_count = 0
-        self.buffer_size = capacity
 
     def add(self, transition: Transition) -> None:
         """
@@ -35,7 +35,7 @@ class ReplayBuffer(AbstractBuffer):
         Args:
             transition (Transition):
         """
-        if self.buffer_count < self.buffer_size:
+        if self.buffer_count < self.capacity:
             self.buffer.append(transition.to_tuple())
             self.buffer_count += 1
         else:
@@ -58,9 +58,9 @@ class ReplayBuffer(AbstractBuffer):
         """
         Count of buffer
         Returns:
-            int: min(buffer_count, buffer)
+            int: min(buffer_count, capacity)
         """
-        return min(self.buffer_count, len(self.buffer))
+        return min(self.buffer_count, self.capacity)
 
     def clear(self) -> None:
         """
