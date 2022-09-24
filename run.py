8a5be7bb303005b3154380ea3b919f67a62e17e1
RektPunk <rektpunk@gmail.com>
2022-09-24 22:08:22 +0900

    feat - change init and run py

--- run.py
+++ run.py
@@ -1,18 +1,23 @@
 from enum import Enum
 import gym
-from models.q_learning.qr_dqn.agent import Agent
+from models import QrDqnAgent
 from variables.gamename import GameName
 from variables.hyperparams.q_learning.qr_dqn import HyperParams
 
 
 def train(
     game_name: str,
+    agent,
     hyper_params: Enum,
 ):
     env = gym.make(game_name)
-    agent = Agent(env, hyper_params)
+    agent = agent(env, hyper_params)
     agent.train()
 
 
 if __name__ == "__main__":
-    train(game_name=GameName.CARTPOLE_V1.value, hyper_params=HyperParams)
+    train(
+        game_name=GameName.CARTPOLE_V1.value,
+        agent=QrDqnAgent,
+        hyper_params=HyperParams,
+    )
