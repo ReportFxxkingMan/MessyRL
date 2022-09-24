35edb12c511c167ed632463388f9819f0e335b30
RektPunk <rektpunk@gmail.com>
2022-09-24 22:18:20 +0900

    feat - change hyperparams path

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from enum import Enum
 import gym
 from models import QrDqnAgent
 from variables.gamename import GameName
-from variables import QrDqnHyperParams
+from variables.hyperparams import QrDqnHyperParams
 from module.schemas.metadata import AbstractAgent
 
 
--- variables/__init__.py
+++
@@ -1,2 +0,0 @@
-from variables.hyperparams.q_learning.c51 import HyperParams as C51HyperParams
-from variables.hyperparams.q_learning.qr_dqn import HyperParams as QrDqnHyperParams
